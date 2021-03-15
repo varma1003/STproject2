a896d07e0800ca2f9bb88b8226356b66d6ccfd9c
DELL <mvvsvarma10@gmail.com>
2021-03-15 21:24:14 +0530

    push

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public Calculator(){
+
+    }
+    public int addition(int a, int b){
+        int add=a+b;
+        return add;
+    }
+    public int subtraction(int a, int b){
+        int sub=a-b;
+        return sub;
+    }
+    public int multiplication(int a, int b){
+        int mul=a*b;
+        return mul;
+    }
+}
